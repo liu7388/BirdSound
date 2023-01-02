43a3bcff37cb35040bf577f5ea84aeff699d048a
liu7388 <ting20040906@gmail.com>
2023-01-03 00:30:46 +0800

    update delete_photo.py

--- delete_photo.py
+++ delete_photo.py
@@ -36,17 +36,19 @@ def compare_images(imageA, imageB, img_path):
 
 
 dir_path = './data/label/'
-labels = {'Anas', 'Hirun', 'Motac', 'Passer'}
-target = {5, 8, 54, 310}
+labels = ['Anas', 'Hirun', 'Motac', 'Passer']
 
-for i in target:
+for i in range(1, 4):
     for j in labels:
         for (root, dirs, files) in os.walk(dir_path + str(j)):
             for Ufile in tqdm(files):
-                img_path = os.path.join(root, Ufile)
-                sensitive_pic = im_read(img_path)
-                sensitive_pic = gray_img(sensitive_pic)
-                target_path = './data/images/target/Anas' + str(i) + '.png'
-                target_pic = im_read(target_path)
-                target_pic = gray_img(target_pic)
-                compare_images(sensitive_pic, target_pic, img_path)
+                try:
+                    img_path = os.path.join(root, Ufile)
+                    sensitive_pic = im_read(img_path)
+                    sensitive_pic = gray_img(sensitive_pic)
+                    target_path = './data/images/target/target' + str(i) + '.png'
+                    target_pic = im_read(target_path)
+                    target_pic = gray_img(target_pic)
+                    compare_images(sensitive_pic, target_pic, img_path)
+                except FileNotFoundError:
+                    pass
